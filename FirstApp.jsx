3e85d9bd6c667cce7af7103cc66bf88c470d591a
APerezBanks <88187553+APerezBanks@users.noreply.github.com>
2022-10-17 14:30:36 +0200

    first commit

---
+++ FirstApp.jsx
@@ -0,0 +1,24 @@
+
+// const getResult = (a, b) => {
+//     return a + b;
+// }
+
+// const newMessage = {
+//     message: 'Hola Mundo',
+//     tittle: 'Hasomo'
+// };                  
+                
+
+export const FirstApp = () => {
+
+
+    return (
+    <>  
+     {/* <h1> { getResult(20,23 ) }  </h1> */}
+     <h1> Hasomo  </h1>
+     {/* <code> { JSON.stringify ( newMessage ) } </code> */}
+      <p> Soy un subtitulo </p>
+
+    </>
+    );
+  }

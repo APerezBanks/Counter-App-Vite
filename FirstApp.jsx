fe29da57cc761b449d358c71cd4889d3e62efcda
APerezBanks <88187553+APerezBanks@users.noreply.github.com>
2022-10-18 14:44:47 +0200

    42. default props

--- FirstApp.jsx
+++ FirstApp.jsx
@@ -1,4 +1,5 @@
-
+import PropTypes from 'prop-types';
+ 
 // const getResult = (a, b) => {
 //     return a + b;
 // }
@@ -7,18 +8,33 @@
 //     message: 'Hola Mundo',
 //     tittle: 'Hasomo'
 // };                  
-                
+            
 
-export const FirstApp = () => {
+export const FirstApp = ( { title, subTitle, name} ) => {
 
+//  console.log (props);
 
     return (
     <>  
-     {/* <h1> { getResult(20,23 ) }  </h1> */}
-     <h1> Hasomo  </h1>
+     <h1>{ title }</h1>
      {/* <code> { JSON.stringify ( newMessage ) } </code> */}
-      <p> Soy un subtitulo </p>
-
+     <p> { subTitle } </p>
+     <p> { name } </p>
     </>
     );
   }
+
+  FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.number,
+  }
+
+  FirstApp.defaultProps = {
+    name: 'Alex Perreo Bankero',
+    subTitle:'Aqui tendria que aparecer 139120-3',
+    title:' Aqui no hay un titulo',
+
+  }
+
+    // para reorganizar por orden alfabetico cmd + p luego escribe ">" y selecciona ordern ascendente
+
--- main.jsx
+++ main.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
-
 import './styles.css';
 
 
 
 ReactDOM.createRoot( document.getElementById( 'root')).render(
     <React.StrictMode>
-        <FirstApp />
+        <FirstApp  />
     </React.StrictMode>
 );
 
